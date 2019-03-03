26207af5aa87ea37b014e49aa882e1e1ef3a07a2
yapelaezp <47278451+yapelaezp@users.noreply.github.com>
2019-03-03 18:58:15 -0500

    Aerolinea v1.0

---
+++ Aerolinea.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Yesid Alejandro Peláez
+ */
+public class Aerolinea {
+    private String nombre;
+    
+    public Aerolinea(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+}
