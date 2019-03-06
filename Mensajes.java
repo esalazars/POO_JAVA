5c1652581df2dad9df9398835a1d828af0e51aa3
Sepulgarin <47259267+Sepulgarin@users.noreply.github.com>
2019-03-05 17:42:03 -0800

    Create Mensajes.java

---
+++ Mensajes.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travelworld;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mensajes {
+
+    public void Mensajes() {
+        Map MensajesEsp = new HashMap();
+        MensajesEsp.put("SeleccionIdioma", "=================================\n=== Bienvenido a Travel World ===\n=================================\n1. Español\n2. Ingles\nSeleccione un idioma:");
+        MensajesEsp.put("Principal", "\n1. Iniciar Sesión.\n2. Registrarse.\n3. Buscar productos.\n4. Salir.\nOpción:");
+        MensajesEsp.put("Principal2", "\n1. Buscar Productos.\n2. Cerrar Sesion.\n3. Modificar Datos.\n");
+        MensajesEsp.put("Principal3", "\nSeleccione lo que desea modificar:\n1. Nombre.\n2. Pais.\n3. Numero telefono.\n4. Fecha de nacimiento.\n5. E-mail\n6. Targeta de credito.\n7. Direccion.\n8. Contraseña.");
+        MensajesEsp.put("Principal4", "\nPor favor escoje lo que deseas buscar:\n1. Vuelos.\n2. Habitaciones de hotel.\n3.Cancelar busqueda.");
+        MensajesEsp.put("CargandoDatos", "Cargando datos del sistema...");
+        MensajesEsp.put("DatosCargados", "Datos cargados exitosamente.");
+        MensajesEsp.put("GuardandoDatos", "Guardando datos...");
+        MensajesEsp.put("DatosGuardados", "Datos guardados exitosamente.");
+        MensajesEsp.put("ErrorCargando", "Error cargando datos del sistema.");
+        MensajesEsp.put("ErrorGuardando", "Error guardando datos del sistema.");
+        MensajesEsp.put("IntenteDeNuevo", "Intente de nuevo.");
+        MensajesEsp.put("Reiniciando", "Reiniciado...");
+        MensajesEsp.put("FinalizandoPrograma", "Finalizando programa...\nAdios.");
+        MensajesEsp.put("LeneInformacion", "Por favor llene la siguiente informacion:");
+        MensajesEsp.put("CuentaCreada", "Cuenta creada exitosamente");
+        MensajesEsp.put("IDExiste", "ID existente. Por favor intente otro.");
+        MensajesEsp.put("PedirID", "Ingrese ID de Usuario:");
+        MensajesEsp.put("PedirNombre", "Ingrese nombre de usuario:");
+        MensajesEsp.put("PedirPais", "Ingrese el Pais:");
+        MensajesEsp.put("PedirContraseña", "Ingrese contraseña:");
+        MensajesEsp.put("TipoDocumento", "Tipo de documento:");
+        MensajesEsp.put("PedirEmail", "Ingresa el E-mail:");
+        MensajesEsp.put("SeccionIniciada", "Sesión iniciada correctamente.\nBienvenido");
+        MensajesEsp.put("ErrorIniciando", "Error iniciando sesión. Por favor intente de nuevo.");
+        MensajesEsp.put("ContraseñaIncorrecta", "Contraseña incorrecta. Por favor intente de nuevo.");
+        MensajesEsp.put("IDNoExiste", "ID inexistente. Por favor intente de nuevo.");
+        MensajesEsp.put("PedirFechaNacimiento", "Ingrese fecha de nacimiento(aaaa/mm/dd):");
+        MensajesEsp.put("ErrorFecha", "Error ingresando fecha, intentelo de nuevo siguiente el formato(aaaa/mm/dd).");
+        MensajesEsp.put("PedirTelefono", "Ingrese numero telefonico:");
+        MensajesEsp.put("PedirDireccion", "Ingrese direccion:");
+        MensajesEsp.put("PedirTarjeta", "Ingrese numero tarjeta bancaria:");
+        MensajesEsp.put("RecuperarContraseña", "Selecciona 1 para recuperar contraseña");
+        MensajesEsp.put("OpcionInvalida", "Opcion invalida. Por favor intente de nuevo.");
+        MensajesEsp.put("PedirRangoPrecio", "Por favor seleccione el rango de precio");
+        MensajesEsp.put("Minimo", "Valor Minimo: ");
+        MensajesEsp.put("Maximo", "Valor maximo: ");
+        MensajesEsp.put("SeleccionarCompra", "Seleccione la id del producto que desea comprar");
+        MensajesEsp.put("ProductoComprado", "Producto comprado exitosamente");
+        MensajesEsp.put("InformacionModificada", "Informacion de la cuenta modificada exitosamente");
+        MensajesEsp.put("SesionCerrada", "Sesion cerrada");
+        MensajesEsp.put("SeleccionHistorial", "4. Mostrar historial de compras");
+        MensajesEsp.put("SeleccionCancelarCompra", "5. Cancelar compra");
+        MensajesEsp.put("ProductoParaCancelar", "Seleccione el producto que desee cancelar");
+        MensajesEsp.put("SeleccionCerrarPrograma", "5. Cerrar Programa");
+        MensajesEsp.put("Factura", "--------------------------");
+        MensajesEsp.put("Subtotal", "Subtotal:             ");
+        MensajesEsp.put("IVA", "IVA:                 ");
+        MensajesEsp.put("Total", "Total:             ");
+        MensajesEsp.put("Vacio", "");
+        MensajesEsp.put("Fecha", "Fecha: ");
+        MensajesEsp.put("NumeroFactura", "Numero de factura ");
+        MensajesEsp.put("Cliente", "Cliente");
+        MensajesEsp.put("Codigo/Valor", "Codigo producto  |   Valor");
+        Map MensajesEng = new HashMap();
+        MensajesEng.put("Principal", "\n1. Log In.\n2. Sign Up.\n3. Search products.\n4. Go Out.\nOption:");
+        MensajesEng.put("Principal2", "\n1. Search Products.\n2. Close Session.\n3.Modify Data.");
+        MensajesEng.put("Principal3", "\nSelect what you want to change:\n1. Name.\n2. Country.\n3. Phone number.\n4 Date of birth.\n5. E-mail.\n6 .Credit card.\n7 Address.\n8. Password.");
+        MensajesEng.put("Principal4", "\nPlease choose what you want to search for:\n1. Flights.\n2. Hotel rooms.\n3.Cancel search.");
+        MensajesEng.put("CargandoDatos", "Loading system data ...");
+        MensajesEng.put("DatosCargados", "Data loaded successfully.");
+        MensajesEng.put("GuardandoDatos", "Saving data ...");
+        MensajesEng.put("DatosGuardados", "Data saved successfully.");
+        MensajesEng.put("ErrorCargando", "Error loading system data.");
+        MensajesEng.put("ErrorGuardando", "Error loading system data.");
+        MensajesEng.put("IntenteDeNuevo", "Try again.");
+        MensajesEng.put("Reiniciando", "Reloading...");
+        MensajesEng.put("FinalizandoPrograma", "Finishing program...\\nBye.");
+        MensajesEng.put("LeneInformacion", "Please provide the following information:");
+        MensajesEng.put("CuentaCreada", "Account created succesfully");
+        MensajesEng.put("IDExiste", "ID already registered. Please try a different one");
+        MensajesEng.put("PedirID", "Enter User ID:");
+        MensajesEng.put("PedirNombre", "Enter username:");
+        MensajesEng.put("PedirPais", "Enter country:");
+        MensajesEng.put("PedirContraseña", "Ingrese contraseña:");
+        MensajesEng.put("TipoDocumento", "Tipo de documento:");
+        MensajesEng.put("PedirEmail", "Ingresa el E-mail:");
+        MensajesEng.put("SeccionIniciada", "Sesión iniciada correctamente.\nBienvenido");
+        MensajesEng.put("ErrorIniciando", "Error iniciando sesión. Por favor intente de nuevo.");
+        MensajesEng.put("ContraseñaIncorrecta", "Contraseña incorrecta. Por favor intente de nuevo.");
+        MensajesEng.put("IDNoExiste", "ID inexistente. Por favor intente de nuevo.");
+        MensajesEng.put("PedirFechaNacimiento", "Enter birthdate(yyyy/mm/dd):");
+        MensajesEng.put("ErrorFecha", "Error entering date, try again following the format yyyy/mm/dd.");
+        MensajesEng.put("PedirTelefono", "Enter phone number:");
+        MensajesEng.put("PedirDireccion", "Enter address:");
+        MensajesEng.put("PedirTarjeta", "Enter bank card number:");
+        MensajesEng.put("RecuperarContraseña", "Press 1 to recover your password");
+        MensajesEng.put("OpcionInvalida", "Invalid input. Please try again.");
+        MensajesEng.put("PedirRangoPrecio", "Please select price range");
+        MensajesEng.put("Minimo", "Min price: ");
+        MensajesEng.put("Maximo", "Max price: ");
+        MensajesEng.put("SeleccionarCompra", "Select product id to purchase");
+        MensajesEng.put("ProductoComprado", "Product purchased succesfully");
+        MensajesEng.put("InformacionModificada", "Account information succesfully modified");
+        MensajesEng.put("SesionCerrada", "Logged out succesfully");
+        MensajesEng.put("SeleccionHistorial", "4. Show purchase history");
+        MensajesEng.put("SeleccionCancelarCompra", "5. Cancel purchase");
+        MensajesEng.put("ProductoParaCancelar", "Please select the purchase you would like to cancel");
+        MensajesEng.put("SeleccionCerrarPrograma", "5. Close Program");
+        MensajesEng.put("Factura", "--------------------------");
+        MensajesEng.put("Subtotal", "Subtotal:             ");
+        MensajesEng.put("IVA", "IVA:                 ");
+        MensajesEng.put("Total", "Total:             ");
+        MensajesEng.put("Vacio", "");
+        MensajesEng.put("Fecha", "Date: ");
+        MensajesEng.put("NumeroFactura", "Receipt no. ");
+        MensajesEng.put("Cliente", "Customer");
+        MensajesEng.put("Codigo/Valor", "Product id  |   Amount Paid");
+
+    }
+
+}
