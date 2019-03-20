fa630220c8af273f78f475d84e864cfe50603fbe
Eladio <esalazars@unal.edu.co>
2019-03-20 08:18:56 -0500

    Creada la clase principal
    Se creo la clase Travel que contendra la funcion main

---
+++ TravelWorld/src/Travel.java
@@ -0,0 +1,17 @@
+/*
+ * Clase principal para ejecutar y probar el programa
+ *
+ */
+
+/**
+ *
+ * @author Travel World
+ */
+//Importar librerias aqui
+
+public class Travel {
+    
+    public static void main(String[] args){
+        System.out.println("Hi Travle World.");
+    }    
+}
