7b5600b478e860f356451558d896368ed9f17299
Cristian <cquinchiar@unal.edu.co>
2019-03-28 12:00:17 -0500

    Cambio2

--- TravelWorld/src/java/controllers/Index.java
+++ TravelWorld/src/java/controllers/Index.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/Index"})
+@WebServlet(urlPatterns = {"/"})
 public class Index extends HttpServlet {
 
     @Override
